5b0c01028f2ac5a03a0f966d31257adee1ade6f5
pe4rl0k <163330893+pe4rl0k@users.noreply.github.com>
2024-05-21 19:22:01 +0200

    change variable

--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -1,33 +1,70 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CustomToken is ERC20 {
+/**
+ * @title Custom ERC20 Token
+ * This contract creates an ERC20 token with specified details.
+ */
+contract CustomERC20Token is ERC20 {
+    /**
+     * @dev Constructor function that initializes the ERC20 token with a name, symbol, and initial supply.
+     * @param tokenName Name of the token.
+     * @param tokenSymbol Symbol of the token.
+     * @param initialSupplyAmount Initial supply of the token, denominated in the smallest unit.
+     */
     constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply
-    ) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+        string memory tokenName,
+        string memory tokenSymbol,
+        uint256 initialSupplyAmount
+    ) ERC20(tokenName, tokenSymbol) {
+        // Mint initial supply to the contract deployer.
+        _mint(msg.sender, initialSupplyAmount);
     }
 }
 
+/**
+ * @title Token Factory
+ * This contract allows for the creation and tracking of multiple CustomERC20Token contracts.
+ */
 contract TokenFactory {
-    address[] public deployedTokens;
+    /**
+     * @dev Array to store addresses of deployed tokens.
+     */
+    address[] public deployedTokenAddresses;
 
-    event TokenCreated(address tokenAddress, string name, string symbol, uint256 initialSupply);
+    /**
+     * @dev Event to notify when a new token is created.
+     * @param newTokenAddress Address of the newly created token.
+     * @param tokenName Name of the token.
+     * @param tokenSymbol Symbol of the token.
+     * @param initialSupply Supply of the token, not adjusted for decimals.
+     */
+    event TokenCreation(address indexed newTokenAddress, string tokenName, string tokenSymbol, uint256 initialSupply);
 
-    function createToken(
+    /**
+     * @dev Function to create a new CustomERC20Token.
+     * @param name The name for the new token.
+     * @param symbol The symbol for the new token.
+     * @param initialSupply The initial supply for the new token, denominated in the token's smallest unit.
+     */
+    function createCustomToken(
         string memory name,
         string memory symbol,
         uint256 initialSupply
     ) public {
-        CustomToken newToken = new CustomToken(name, symbol, initialSupply * (10**18));
-        deployedTokens.push(address(newToken));
-        emit TokenCreated(address(newToken), name, symbol, initialSupply);
+        CustomERC20Token newToken = new CustomERC20Token(name, symbol, initialSupply * (10**18));
+        deployedTokenAddresses.push(address(newToken));
+
+        emit TokenCreation(address(newToken), name, symbol, initialSupply);
     }
 
-    function getDeployedTokens() public view returns (address[] memory) {
-        return deployedTokens;
+    /**
+     * @dev Getter function for retrieving addresses of all deployed tokens.
+     * @return An array of addresses for each deployed token.
+     */
+    function getAllDeployedTokens() public view returns (address[] memory) {
+        return deployedTokenAddresses;
     }
 }
